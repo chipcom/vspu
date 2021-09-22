e7012e546498fe6db00ca968c745ae078f5e142e
chipcom <vbaykin@mail.ru>
2021-09-22 12:44:51 +0300

    использование pprint36

--- test.py
+++ test.py
@@ -8,15 +8,17 @@
     # print(response.text)
 # else:
     # print(f'Server error: {response.status_code}')
+
+import pprint
 from testDB import myDB as db
 
-print(db.get_name_module('Б1.О.01'))
-print(db.get_name_course('Б1.О.02.01'))
-print(db.get_list_courses_module('Б1.О.01'))
-print(db.get_name_competence('ОПК-1'))
-print(db.get_name_indicator('ИОПК-6.3'))
-print(db.get_list_indicators_competence('УК-1'))
-print(db.get_list_competences_course('Б1.О.01.01'))
+pprint.pprint(db.get_name_module('Б1.О.01'))
+pprint.pprint(db.get_name_course('Б1.О.02.01'))
+pprint.pprint(db.get_list_courses_module('Б1.О.01'))
+pprint.pprint(db.get_name_competence('ОПК-1'))
+pprint.pprint(db.get_name_indicator('ИОПК-6.3'))
+pprint.pprint(db.get_list_indicators_competence('УК-1'))
+pprint.pprint(db.get_list_competences_course('Б1.О.01.01'))
 # error
 print(db.get_name_module('Б1.О.10'))
 print(db.get_name_course('Б1.О.02.25'))
